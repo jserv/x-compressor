8d328e9f07919ed41fc4db35dd4521afbf61cfa5
Jim Huang <jserv.tw@gmail.com>
2020-09-11 05:07:39 +0800

    Remove duplicated comment

--- libx.c
+++ libx.c
@@ -95,7 +95,6 @@ static inline size_t ctzu32(uint32_t n)
     static const int lut[32] = {0,  1,  28, 2,  29, 14, 24, 3,  30, 22, 20,
                                 15, 25, 17, 4,  8,  31, 27, 13, 23, 21, 19,
                                 16, 7,  26, 12, 18, 6,  11, 5,  10, 9};
-    /* http://graphics.stanford.edu/~seander/bithacks.html */
     return lut[((uint32_t)((n & -n) * 0x077CB531U)) >> 27];
 }
 
